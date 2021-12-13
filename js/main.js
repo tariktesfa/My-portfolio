6c7f53c8b8a8d4b6125ac6399f0007804156182d
tariktesfa <tariktesfa9090@gmail.com>
2021-12-13 19:42:35 +0300

    Fix eslint error

--- js/main.js
+++ js/main.js
@@ -2,11 +2,10 @@ const menuItem = document.querySelector('.mobile-item');
 const closeMobileItems = document.querySelectorAll('.closeMenu');
 const displayMobileMenu = () => {
   menuItem.style.display = 'block';
-}
+};
 const closeMobileMenu = () => {
   menuItem.style.display = 'none';
-}
-
+};
 document.querySelector('.openMenu').addEventListener('click', displayMobileMenu);
 closeMobileItems.forEach(element => {
   element.addEventListener('click', closeMobileMenu)
