cb113faebece3a97c41a9bac1a58d2885cb97d08
tariktesfa <tariktesfa9090@gmail.com>
2021-12-13 18:43:31 +0300

    Add mobile menu

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let menuItem = document.querySelector('.mobile-item');
+const closeMobileItems = document.querySelectorAll('.closeMenu');
+const displayMobileMenu = () => {
+  menuItem.style.display = "block";
+}
+
+const closeMobileMenu = () => {
+  menuItem.style.display = "none";
+}
+
+document.querySelector('.openMenu').addEventListener('click',displayMobileMenu)
+closeMobileItems.forEach(element => {
+  element.addEventListener('click',closeMobileMenu)
+});
