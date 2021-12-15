82eabb079b10d967d7f18465a65ca45530eb5b62
Aime Malaika <aimemalaika1995@gmail.com>
2021-12-15 17:54:41 +0200

    Fix marging on technologies list

--- js/main.js
+++ js/main.js
@@ -81,33 +81,63 @@ for (let i = 0; i < projectDetail.length; i += 1) {
     experienceList += (k === 0) ? `<li><a href="#" class="canopy">${projectDetail[i].exprience[k]}</a></li>` : `<li><a href="#">${projectDetail[i].exprience[k]}</a></li>`;
     if (k !== projectDetail[i].exprience.length - 1) experienceList += '<li><a href="#"><img src="./images/Counter.png" alt="dot"></a></li>';
   }
+  if (i%2 == 0) {
     document.querySelector('#portfolio').innerHTML += `<div class="main-container">
-  <div class="grid-item" id="work-1">
-    <div class="flex-item1"><img src="${projectDetail[i].imageMobile}" alt="my tonic project"></div>
-    <div class="desk-item1"><img src="${projectDetail[i].image}" alt="my tonic project"></div>
-    <div class="desk-col">
-      <div class="flex-item2">
-        <h2>${projectDetail[i].title}</h2>
+      <div class="grid-item" id="work-1">
+        <div class="flex-item1"><img src="${projectDetail[i].imageMobile}" alt="my tonic project"></div>
+        <div class="desk-item1"><img src="${projectDetail[i].image}" alt="my tonic project"></div>
+        <div class="desk-col">
+          <div class="flex-item2">
+            <h2>${projectDetail[i].title}</h2>
+          </div>
+          <div class="flex-item2-desk">
+            <h2>${projectDetail[i].title}</h2>
+          </div>
+          <div class="flex-item3">
+            <ul class="inner-element">
+              ${experienceList}
+            </ul>
+          </div>
+          <div class="flex-item4">
+            <p>${projectDetail[i].desription}</p>
+          </div>
+          <div class="flex-item5">
+            <ul class="inner-flex-item">
+            ${tech}
+            </ul>
+          </div>
+          <div class="flex-item6"><button data-index="${i}" class="see-project">See project</button></div>
+        </div>
+      </div>`;
+  } else {
+    document.querySelector('#portfolio').innerHTML += `<div class="main-container">
+    <div class="grid-item" id="work-1">
+      <div class="flex-item1"><img src="${projectDetail[i].imageMobile}" alt="my tonic project"></div>
+      <div class="desk-col">
+        <div class="flex-item2">
+          <h2>${projectDetail[i].title}</h2>
+        </div>
+        <div class="flex-item2-desk">
+          <h2>${projectDetail[i].title}</h2>
+        </div>
+        <div class="flex-item3">
+          <ul class="inner-element">
+            ${experienceList}
+          </ul>
+        </div>
+        <div class="flex-item4">
+          <p>${projectDetail[i].desription}</p>
+        </div>
+        <div class="flex-item5">
+          <ul class="inner-flex-item">
+          ${tech}
+          </ul>
+        </div>
+        <div class="flex-item6"><button data-index="${i}" class="see-project">See project</button></div>
       </div>
-      <div class="flex-item2-desk">
-        <h2>${projectDetail[i].title}</h2>
-      </div>
-      <div class="flex-item3">
-        <ul class="inner-element">
-          ${experienceList}
-        </ul>
-      </div>
-      <div class="flex-item4">
-        <p>${projectDetail[i].desription}</p>
-      </div>
-      <div class="flex-item5">
-        <ul class="inner-flex-item">
-        ${tech}
-        </ul>
-      </div>
-      <div class="flex-item6"><button data-index="${i}" class="see-project">See project</button></div>
-    </div>
-  </div>`;
+      <div class="desk-item1"><img src="${projectDetail[i].image}" alt="my tonic project"></div>
+    </div>`;
+  }
 }
 /* End of project detail population when the pages loads */
 
